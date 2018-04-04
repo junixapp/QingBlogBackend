4894cd82a5d633c3bc46e27cf949facc654ead38
li-xiaojun <16167479@qq.com>
2018-04-04 19:03:21 +0800

    将权限检查从auth_handler中单独抽取出来

---
+++ middleware/permission_handler.js
@@ -0,0 +1,16 @@
+'use strict'
+// 权限检查中间件
+//是否需要检查权限
+function isNeedPermission(ctx) {
+    return ctx.path.startsWith("/users") && ctx.method==="DELETE"
+}
+module.exports = ()=> async (ctx, next) => {
+    //checkPermission
+    if(isNeedPermission(ctx)){
+        if(!ctx.state.auth.isAdmin){
+            throw PermissionDenyError
+        }
+    }
+
+    await next()
+}
