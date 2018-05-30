446ce1d33bb7df0c8d214c39b5b3308daa5da29d
li-xiaojun <16167479@qq.com>
2018-05-30 18:18:39 +0800

    enhance config grace

---
+++ config/dev.js
@@ -0,0 +1,9 @@
+'use strict'
+module.exports = {
+    JWT_SECRET: 'QingBlog',
+    JWT_EXPIRE: 3600 * 24 , //a month
+
+    DB_NAME: 'QingBlog',
+
+    APP_PORT: 80,
+}
---
+++ config/index.js
@@ -0,0 +1,10 @@
+'use strict'
+
+let config = null;
+if(process.env.NODE_ENV==='production'){
+    config = require('./prod')
+}else {
+    config = require('./dev')
+}
+
+module.exports = config
